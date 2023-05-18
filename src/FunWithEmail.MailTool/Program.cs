bdfb87733203f9007028dfe757847606c72c073a
Dylan Beattie <dylan@dylanbeattie.net>
2023-05-18 11:43:56 +0100

    Update build to use /src

--- src/FunWithEmail.MailTool/Program.cs
+++ src/FunWithEmail.MailTool/Program.cs
@@ -1,6 +1,9 @@
+using MailKit.Net.Smtp;
 using Microsoft.Extensions.Configuration;
-using System.Net.Mail;
-using System.Net;
+using MimeKit;
+using MimeKit.Utils;
+using Mjml.Net;
+
 
 IConfiguration config = new ConfigurationBuilder()
 	.AddJsonFile("appSettings.json")
@@ -8,15 +11,42 @@ IConfiguration config = new ConfigurationBuilder()
 	.AddUserSecrets(typeof(Program).Assembly)
 	.Build();
 
-var from = "hello@funwith.email";
-var smtpServers = config.GetSection("Smtp").Get<SmtpSettings[]>();
-var recipients = new[] { "dylan@fm.funwith.email", "dylan@dylanbeattie.net", "dylan.beattie@gmail.com" };
-foreach (var server in smtpServers) {
-	var client = server.CreateClient();
+var from = new MailboxAddress("Company Logo", "hello@funwith.email");
+// var from = new MailboxAddress("Company Logo", "do-not-reply@company-logo.com");
+var smtpServers = config.GetSection("Smtp").Get<Dictionary<string, SmtpSettings>>();
+var recipients = new[] { "dylan@dylanbeattie.net" }; //  "dylan.beattie@gmail.com", "dylan@funwith.email", "dylan@fm.funwith.email", };
+var renderer = new MjmlRenderer();
+var server = smtpServers["sendgrid.com"];
+// foreach (var server in smtpServers.Values) {
+	var smtp = new SmtpClient();
+	smtp.Connect(server.Host, server.Port);
+	if (server.Username != null) smtp.Authenticate(server.Username, server.Password);
 	foreach (var rcpt in recipients) {
-		client.Send(from, rcpt, $"Test message from {from} to {rcpt} via {server}", $"Test email from {from} to {rcpt} via {server}");
+		var mail = new MimeMessage();
+		// mail.Subject = "Fun with Email!";
+		mail.Subject = "Welcome to Company Logo";
+		mail.From.Add(from);
+		mail.To.Add(new MailboxAddress(rcpt, rcpt));
+		var bb = new BodyBuilder();
+		var text = $"Test email from {from} to {rcpt} via {server}";
+		bb.TextBody = text;
+//		var mjml = File.ReadAllText("Templates/Mail.mjml");
+		var mjml = File.ReadAllText("Templates/CompanyLogo.mjml");
+		var (html, errors) = renderer.Render(mjml);
+		var logo = bb.LinkedResources.Add("company-logo-dark-mode.png");
+		// var logo = bb.LinkedResources.Add("funwithemail.png");
+		logo.ContentId = MimeUtils.GenerateMessageId();
+		html = html.Replace("__LOGO_CONTENT_ID__", logo.ContentId);
+		html = html.Replace("__RECIPIENT__", rcpt);
+		html = html.Replace("__SMTP_RELAY__", server.ToString());
+		bb.HtmlBody = html;
+		mail.Body = bb.ToMessageBody();
+		smtp.Send(mail);
 		Console.WriteLine($"Sent to {rcpt} via {server}");
 	}
-}
+	smtp.Disconnect(true);
+// }
 Console.WriteLine("Done! Press any key to exit.");
 Console.ReadKey(true);
+
+
--- src/FunWithEmail.MailTool/SmtpSettings.cs
+++ src/FunWithEmail.MailTool/SmtpSettings.cs
@@ -1,6 +1,3 @@
-using System.Net;
-using System.Net.Mail;
-
 public class SmtpSettings {
 	public string Host { get; set; } = "";
 	public int Port { get; set; } = 25;
@@ -8,12 +5,4 @@ public class SmtpSettings {
 	public string? Password { get; set; }
 	public bool EnableSsl { get; set; }
 	public override string ToString() => $"{Host}:{Port}";
-
-	public SmtpClient CreateClient() {
-		var client = new SmtpClient(Host, Port);
-		if (String.IsNullOrEmpty(Username)) return client;
-		client.Credentials = new NetworkCredential(Username, Password);
-		if (EnableSsl) client.EnableSsl = true;
-		return client;
-	}
 }
